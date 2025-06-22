466a6ae49d6ef7708354e661069fda43120d7a3f
saregokcan <gokcansare@gmail.com>
2025-06-22 16:08:22 +0300

    .

--- js/task-3.js
+++ js/task-3.js
@@ -14,12 +14,3 @@ const profile = {
     return `${this.username} has ${this.playTime} active hours!`;
   }
 };
-
-// Testler
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
